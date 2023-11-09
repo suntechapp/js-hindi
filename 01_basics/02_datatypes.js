4471fd97905f7d2dc396929c0395103ccb7c7061
Smriti Yadav <smriti.yadav20@gmail.com>
2023-11-09 12:09:14 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+// alert(3+3) we are using node js not browser
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof "smriti") //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
